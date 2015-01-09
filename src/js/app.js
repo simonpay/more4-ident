49c1ab38056d16e0fa9eb843d9f7a4602255397b
Simon Pay <simonpay@yahoo.co.uk>
2015-01-09 23:22:01 +0000

    Working ident but needs work on the start - need to add animated triangles that open out rather than have them appear statically

--- src/js/app.js
+++ src/js/app.js
@@ -1,184 +1,422 @@
 $(function(){
 
-	var $flipContainer = $(".flip-container"),
-		aryFlipContainers = [
-		    { 
-		    	positionID: "2-1", 
-		    	delay: "250", 
-		    	flipCards: [
-		    		{
-		    			className: "light-blue"
-		    		},
-		    		{
-		    			className: "peach"
-		    		},
-		    		{
-		    			className: "maroon"
-		    		},
-		    		{
-		    			className: "green"
-		    		},
-		    		{
-		    			className: "grey"
-		    		},
-		    	]
-		    },
-		    { 
-		    	positionID: "3-1", 
-		    	delay: "500", 
-		    	flipCards: [
-		    		{
-		    			className: "peach"
-		    		},
-		    		{
-		    			className: "mint-green"
-		    		},
-		    		{
-		    			className: "blue"
-		    		},
-		    		{
-		    			className: "peach"
-		    		},
-		    		{
-		    			className: "maroon flip-card--dont-animate"
-		    		},
-		    	]
-		    },
-		    { 
-		    	positionID: "1-2", 
-		    	delay: "1000", 
-		    	flipCards: [
-		    		{
-		    			className: "yellow"
-		    		},
-		    		{
-		    			className: "pink"
-		    		},
-		    		{
-		    			className: "dark-blue"
-		    		},
-		    		{
-		    			className: "light-blue"
-		    		},
-		    		{
-		    			className: "orange"
-		    		},
-		    	]
-		    },
-		    { 
-		    	positionID: "2-2", 
-		    	delay: "750", 
-		    	// delay: "1",
-		    	flipCards: [
-		    		{
-		    			className: "light-blue"
-		    		},
-		    		{
-		    			className: "blue"
-		    		},
-		    		{
-		    			className: "yellow"
-		    		},
-		    		{
-		    			className: "maroon"
-		    		},
-		    		{
-		    			className: "green"
-		    		},
-		    		{
-		    			className: "light-blue flip-card--dont-animate"
-		    		},
-		    	]
-		    },
-		    { 
-		    	positionID: "3-2", 
-		    	delay: "1000", 
-		    },
-		    { 
-		    	positionID: "1-3", 
-		    	delay: "250", 
-		    },
-		    { 
-		    	positionID: "2-3", 
-		    	delay: "1250", 
-		    },
-		    { 
-		    	positionID: "3-3", 
-		    	delay: "750", 
-		    },
-		    { 
-		    	positionID: "3-4", 
-		    	delay: "250", 
-		    },
-		];
+    var $flipContainer = $(".flip-container"),
+        aryFlipContainers = [
+            { 
+                index: "1", 
+                positionID: "2-1", 
+                delay: "250", 
+                flipCards: [
+                    {
+                        color: "sky"
+                    },
+                    {
+                        color: "ham"
+                    },
+                    {
+                        color: "plum"
+                    },
+                    {
+                        color: "parsley"
+                    },
+                    {
+                        color: "dusk"
+                    },
+                ]
+            },
+            { 
+                index: "2", 
+                positionID: "3-1", 
+                delay: "500", 
+                flipCards: [
+                    {
+                        color: "ham"
+                    },
+                    {
+                        color: "aqua"
+                    },
+                    {
+                        color: "ocean"
+                    },
+                    {
+                        color: "ham"
+                    },
+                    {
+                        color: "plum flip-card--dont-animate",
+                        dontAnimate: true
+                    },
+                ]
+            },
+            { 
+                index: "3", 
+                positionID: "1-2", 
+                delay: "1000", 
+                flipCards: [
+                    {
+                        color: "citron"
+                    },
+                    {
+                        color: "pink"
+                    },
+                    {
+                        color: "waves"
+                    },
+                    {
+                        color: "sky"
+                    },
+                    {
+                        color: "mandarin"
+                    },
+                ]
+            },
+            { 
+                index: "4", 
+                positionID: "2-2", 
+                delay: "750", 
+                flipCards: [
+                    {
+                        color: "sky"
+                    },
+                    {
+                        color: "ocean"
+                    },
+                    {
+                        color: "citron"
+                    },
+                    {
+                        color: "plum"
+                    },
+                    {
+                        color: "parsley"
+                    },
+                    {
+                        color: "sky",
+                        dontAnimate: true
+                    },
+                ]
+            },
+            { 
+                index: "5", 
+                positionID: "3-2", 
+                delay: "1000", 
+                flipCards: [
+                    {
+                        color: "mandarin"
+                    },
+                    {
+                        color: "kite"
+                    },
+                    {
+                        color: "parsley"
+                    },
+                    {
+                        color: "plum"
+                    },
+                    {
+                        color: "navy"
+                    },
+                    {
+                        color: "citron",
+                        dontAnimate: true
+                    },
+                ]
+            },
+            { 
+                index: "6", 
+                positionID: "1-3", 
+                delay: "250", 
+                letter: "M", 
+                flipCards: [
+                    {
+                        color: "rose"
+                    },
+                    {
+                        color: "apple"
+                    },
+                    {
+                        color: "aqua"
+                    },
+                    {
+                        color: "kite"
+                    },
+                    {
+                        color: "rose",
+                        dontAnimate: true
+                    },
+                ]
+            },
+            { 
+                index: "7", 
+                positionID: "2-3", 
+                delay: "1250", 
+                letter: "O", 
+                flipCards: [
+                    {
+                        color: "apple"
+                    },
+                    {
+                        color: "dusk"
+                    },
+                    {
+                        color: "citron"
+                    },
+                    {
+                        color: "waves"
+                    },
+                    {
+                        color: "aqua",
+                        dontAnimate: true
+                    },
+                ]
+            },
+            { 
+                index: "8", 
+                positionID: "3-3", 
+                delay: "750", 
+                letter: "R", 
+                flipCards: [
+                    {
+                        color: "citron"
+                    },
+                    {
+                        color: "apple"
+                    },
+                    {
+                        color: "sky"
+                    },
+                    {
+                        color: "apple"
+                    },
+                    {
+                        color: "ocean",
+                        dontAnimate: true
+                    },
+                ]
+            },
+            { 
+                index: "9", 
+                positionID: "4-3", 
+                delay: "250", 
+                letter: "E", 
+                flipCards: [
+                    {
+                        color: "elephant"
+                    },
+                    {
+                        color: "elephant"
+                    },
+                    {
+                        color: "elephant"
+                    },
+                    {
+                        color: "elephant"
+                    },
+                    {
+                        color: "elephant"
+                    },
+                    {
+                        color: "elephant",
+                        dontAnimate: true
+                    },
+                ]
+            },
+            { 
+                index: "10", 
+                positionID: "3-4", 
+                delay: "250", 
+                flipCards: [
+                    {
+                        color: "kite"
+                    },
+                    {
+                        color: "dusk"
+                    },
+                    {
+                        color: "parsley"
+                    },
+                    {
+                        color: "plum"
+                    },
+                    {
+                        color: "hibiscus"
+                    },
+                ]
+            },
+        ];
 
 
-	// var cloneFlipContainerDelay = function (flipContainerIndex, positionID, delay) {
+    var removeLetters = function () {
+        // console.log($(this).parent().parent().hasClass( "delay-250" ));
 
-	// 	setTimeout(function(){
+        var $flipContainer = $(this).parent().parent();
 
-	// 		console.log("called: " + flipContainerIndex);
+        // if parent flipcard el does NOT have class of "hasLetter", remove letter svg/polygon etc
+        if(!$flipContainer.hasClass( "hasLetter" )) {
+            $(this).remove();
+        }else{
+            // var letter = $flipContainer.attr("class");
+            if($flipContainer.hasClass( "hasLetter--M" )) {
 
-	// 		$flipContainer
-	// 			.clone()
-	// 			.appendTo( ".viewport" )
-	// 			.addClass( "flip-container--" + positionID + " delay-" + delay);
+                // console.log($flipContainer.find(".letter-left--letter-o"));
 
-	// 			var $flipCard = $(".flip-container--" + positionID + " .flip-card");
+                // remove O, R, E
+                // console.log($flipContainer.find("letter-left--letter-o"));
+                // console.log($(this).attr("class"));
+                // console.log($flipContainer);
 
-	// 			if (aryFlipContainers[flipContainerIndex].flipCards) {
+                // if($flipContainer.find(".letter-left--letter-o")) {
+                if($(this).hasClass("letter-left--letter-o")) {
+                    // console.log($flipContainer.find(".letter-left--letter-o"));
+                    // console.log("GO REMOVE");
+                    // $(this).remove();
+                }
 
-	// 				for (var j = 0; j < aryFlipContainers[flipContainerIndex].flipCards.length; j++) {
-
-	// 					// setTimeout(cloneFlipContainerDelay(j, positionID, aryFlipContainers[flipContainerIndex].flipCards[j].className), 5000);
-
-	// 					$flipCard
-	// 						.clone()
-	// 						.appendTo( ".flip-container--" + positionID )
-	// 						.attr("class", "flip-card flip-card--" + (j+1) + " " + aryFlipContainers[flipContainerIndex].flipCards[j].className);
-
-	// 				}
-	// 			}
-
-	// 			$flipCard.remove();
-
-
-	// 	}, delay);
-
-	// };
+                // if $(this) (the current letter svg path) has a class of "letter-left--letter-o", remove $(this)
+                if ($(this).hasClass("letter-left--letter-o")) {
+                    // console.log($(this).attr("class"));
+                    // $(this).remove();
+                }
+                // $flipContainer.find("letter-left--letter-o, letter-right--letter-o, letter-left--letter-r, letter-right--letter-r, letter-left--letter-e, letter-right--letter-e")
+                //     .remove();
+            }
+        }
+        
 
 
-	for (var i = 0; i < aryFlipContainers.length; i++) {
 
-		// var cloneFlipContainerDelay = function (flipContainerIndex, positionID, delay) {
-		// cloneFlipContainerDelay(i, aryFlipContainers[i].positionID, aryFlipContainers[i].delay);
-		
-		$flipContainer
-			.clone()
-			.appendTo( ".viewport" )
-			.addClass( "flip-container--" + aryFlipContainers[i].positionID + " delay-" + aryFlipContainers[i].delay);
+        // remove triangle svg from E flip-card
+        // not used as we ned the triangle to be there to create the visual animation flip of the letter E
+        if($flipContainer.hasClass( "flip-container--4-3" )) {
+            // $(this).remove();
+            if($flipContainer.find(".triangle")){
+                // console.log("has triangle");
+                // $flipContainer.find(".triangle").remove();
+            }
+            // console.log("TRUE");
+        }
+    };
 
-					 
-			var $flipCard = $(".flip-container--" + aryFlipContainers[i].positionID + " .flip-card");
 
-			if (aryFlipContainers[i].flipCards) {
+    for (var i = 0; i < aryFlipContainers.length; i++) {
 
-				for (var j = 0; j < aryFlipContainers[i].flipCards.length; j++) {
+        var letter, hasLetter;
+        if (aryFlipContainers[i].letter) {
+            letter = aryFlipContainers[i].letter;
+            hasLetter = " hasLetter hasLetter--" + letter;
+        } else {
+            hasLetter = "";
+        }
 
-					// setTimeout(cloneFlipContainerDelay(j, aryFlipContainers[i].positionID, aryFlipContainers[i].flipCards[j].className), 5000);
+        $flipContainer
+            .clone()
+            .appendTo( ".viewport" )
+            .addClass( "flip-container--" + aryFlipContainers[i].positionID + " delay-" + aryFlipContainers[i].delay + hasLetter);
 
-					$flipCard
-						.clone()
-						.appendTo( ".flip-container--" + aryFlipContainers[i].positionID )
-						.attr("class", "flip-card flip-card--" + (j+1) + " " + aryFlipContainers[i].flipCards[j].className);
+            var $flipCard = $(".flip-container--" + aryFlipContainers[i].positionID + " .flip-card");
 
-				}
-			}
+            if (aryFlipContainers[i].flipCards) {
 
-			$flipCard.remove();
+                // var flipCardFirstClass;
+                // if (i===0 || i===2) {
+                //     flipCardFirstClass = "flip-card--first-reverse";
+                // }else{
+                //     flipCardFirstClass = "flip-card--first";
+                // }
 
-	}
-	
-	$flipContainer.remove();
+                // clone a single flip-card for the first part of the flip
+                // $flipCard
+                //     .clone()
+                //     .appendTo( ".flip-container--" + aryFlipContainers[i].positionID )
+                //     .attr("class", "flip-card " + flipCardFirstClass + " " + aryFlipContainers[i].flipCards[0].color)
+                //     .find(".letter-left, .letter-right")
+                //     .each(removeLetters);
+
+                for (var j = 0; j < aryFlipContainers[i].flipCards.length; j++) {
+
+
+                    var dontAnimateClass;
+                    if (aryFlipContainers[i].flipCards[j].dontAnimate) {
+                        dontAnimateClass = " flip-card--dont-animate";
+                    }else{
+                        dontAnimateClass = "";
+                    }
+                    // now clone the rest of the flip-cards
+                    $flipCard
+                        .clone()
+                        .appendTo( ".flip-container--" + aryFlipContainers[i].positionID )
+                        // .attr("class", "flip-card flip-card--" + (j+1) + " " + aryFlipContainers[i].flipCards[j].color)
+                        .attr("class", "flip-card flip-card--" + (j+1) + dontAnimateClass)
+                            .find(".triangle")
+                            .attr("class", "triangle " + aryFlipContainers[i].flipCards[j].color)
+                            .end()
+                                .find(".letter-left, .letter-right")
+                                .each(removeLetters);
+                }
+            }
+            $flipCard.remove();
+    }
+    
+    $flipContainer.remove();
+
+    var selectorString = "",
+        word = "MORE";
+
+    // for (var k = 0; k < word.length; k++) {
+    //     selectorString += ".hasLetter--" + word.substring(k, k+1) + " .letter-left--letter-o, .hasLetter--" + word.substring(k, k+1) + " .letter-right--letter-o, ";
+    // }
+    // console.log("selectorString = " + selectorString);
+
+    // TODO - make this dynamic using a loop
+    // M
+    selectorString += ".hasLetter--M .letter-left--letter-o, .hasLetter--M .letter-right--letter-o, ";
+    selectorString += ".hasLetter--M .letter-left--letter-r, .hasLetter--M .letter-right--letter-r, ";
+    selectorString += ".hasLetter--M .letter-left--letter-e, .hasLetter--M .letter-right--letter-e, ";
+    // O
+    selectorString += ".hasLetter--O .letter-left--letter-m, .hasLetter--O .letter-right--letter-m, ";
+    selectorString += ".hasLetter--O .letter-left--letter-r, .hasLetter--O .letter-right--letter-r, ";
+    selectorString += ".hasLetter--O .letter-left--letter-e, .hasLetter--O .letter-right--letter-e, ";
+    // R
+    selectorString += ".hasLetter--R .letter-left--letter-m, .hasLetter--R .letter-right--letter-m, ";
+    selectorString += ".hasLetter--R .letter-left--letter-o, .hasLetter--R .letter-right--letter-o, ";
+    selectorString += ".hasLetter--R .letter-left--letter-e, .hasLetter--R .letter-right--letter-e, ";
+    // E
+    selectorString += ".hasLetter--E .letter-left--letter-m, .hasLetter--E .letter-right--letter-m, ";
+    selectorString += ".hasLetter--E .letter-left--letter-o, .hasLetter--E .letter-right--letter-o, ";
+    selectorString += ".hasLetter--E .letter-left--letter-r, .hasLetter--E .letter-right--letter-r";
+    $(selectorString).remove();
+
+    // setTimeout(function(){
+    //  $(".flip-card").show();
+    // }, 2000);
+
+
+    $(".flip-card--first-reverse").bind('oanimationend animationend webkitAnimationEnd', function() { 
+        // console.log("animation ended");
+        // console.log(this);
+        // console.log($(this).parent().find(".flip-card:not(.flip-card--first-reverse)"));
+        $(this)
+            .parent()
+                .find(".flip-card")
+                    // .show()
+                    // .parent()
+                    // .find(".flip-card--first-reverse")
+                            // .hide()
+                            // .css("-webkit-animation-play-state", "running")
+                            // .find(".letter-right")
+                            //     .css("-webkit-animation-play-state", "running")
+                            ;
+
+    });
+
+    $(".flip-card--first").bind('oanimationend animationend webkitAnimationEnd', function() { 
+        $(this)
+            .parent()
+                .find(".flip-card:not(.flip-card--first)")
+                    // .show()
+                    // .find(".letter-right")
+                    //     .css("-webkit-animation-play-state", "running")
+                        ;
+
+    });
 
 });
